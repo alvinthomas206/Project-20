d1b9af49bff7c707075953398fb3a01ec8e95ec4
ALVIN THOMAS <alvinthomas206@gmail.com>
2020-11-13 15:27:15 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -30,10 +30,10 @@ var deformation=0.5 *weight *speed *speed/22500;
 if(deformation>180){
   car.shapeColor=color(255,0,0);
 }
-if(deformation>180 && deformation>100){
+if(deformation‹180 && deformation>100){
   car.shapeColor=color(230,230.0);
 }
-if(deformation>100){
+if(deformation‹100){
   car.shapeColor=color(0,255,0);
 }
 
